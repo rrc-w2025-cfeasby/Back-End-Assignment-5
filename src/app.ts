d4fb1c19abd3be602ae8afbb054caf39ba33c51b
Chris Feasby <cfeasby@academic.rrc.ca>
2026-03-05 09:23:11 -0600

    Add dotenv to app and update firebaseConfig.ts to use env instead of service account directly

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Express } from "express";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use("/api/v1", eventRoutes);
