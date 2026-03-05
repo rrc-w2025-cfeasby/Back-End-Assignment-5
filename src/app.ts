d7c04917c17bc4d4ffee25b489f5a18781fbfd36
Chris Feasby <cfeasby@academic.rrc.ca>
2026-03-05 10:10:42 -0600

    Install helmet and set up helmetConfig to run in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Express } from "express";
+import { getHelmetConfig } from "../config/helmetConfig";
 import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
 const app: Express = express();
+
+app.use(getHelmetConfig());
 
 app.use(express.json());
 app.use("/api/v1", eventRoutes);
@@ -13,7 +16,7 @@ app.use("/api/v1", healthRoutes);
 
 // Define a route
 app.get("/", (req, res) => {
-    res.send("Hello, Sergei, welcome to Assignment 3!");
+    res.send("Hello, World!");
 });
 
 export default app;
